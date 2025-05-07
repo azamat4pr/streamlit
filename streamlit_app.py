cbc01b43a7fad37985a94d83b79989e26cfead07
azamat4pr <azamat.jauysh@gmail.com>
2025-05-07 13:40:39 +0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,7 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 @st.cache_data
 def load_data(nrows):
